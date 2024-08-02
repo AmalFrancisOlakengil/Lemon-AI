6cf4cd8c6ac9d2d8f885b1c6aeedd1d7c8b332a2
AmalFrancisOlakengil <143199482+AmalFrancisOlakengil@users.noreply.github.com>
2024-08-02 23:19:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import speech_recognition as sr
 from gtts import gTTS
 import os
 import webbrowser
+from tkinter import *
+import sqlite3
 
 def listen():
     recognizer = sr.Recognizer()
@@ -18,23 +20,34 @@ def listen():
         except sr.RequestError as e:
             print(f"Could not request results; {e}")
             return ""
-def open_application(a):
-     if a == "open opera":
-            os.startfile(r"C:\Users\franc\AppData\Local\Programs\Opera\opera.exe") 
-     elif a == "open visual studio code":
-            os.startfile(r"C:\Users\franc\AppData\Local\Programs\Microsoft VS Code\Code.exe") 
-     elif a == "open edge":
-            os.startfile(r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe") 
-     elif a == "open youtube":
-              webbrowser.open("https://www.youtube.com/")
-     elif a == "open github":
-              webbrowser.open("https://github.com/")
-     elif a == "open chat gpt":
-              webbrowser.open("https://chatgpt.com/")
-     choice = input("Repeat?(y/n)")
-     if(choice == "y"):
-         app_name = listen()  
-         open_application(str(app_name))
-app_name = listen()
-open_application(str(app_name))
+
+def open_application(app_name):
+    if app_name == "open opera":
+        os.startfile(r"C:\Users\franc\AppData\Local\Programs\Opera\opera.exe") 
+    elif app_name == "open visual studio code":
+        os.startfile(r"C:\Users\franc\AppData\Local\Programs\Microsoft VS Code\Code.exe") 
+    elif app_name == "open edge":
+        os.startfile(r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe") 
+    elif app_name == "open youtube":
+        webbrowser.open("https://www.youtube.com/")
+    elif app_name == "open github":
+        webbrowser.open("https://github.com/")
+    elif app_name == "open chat gpt":
+        webbrowser.open("https://chatgpt.com/")
+    else:
+        print("Application not recognized")
+    
+def start_listening():
+    app_name = listen()
+    open_application(app_name)
+
+window = Tk()
+window.geometry("600x400")
+window.title("Voice Command App")
+
+listen_button = Button(window, text="Start Listening", command=start_listening)
+listen_button.pack(pady=20)
+
+window.mainloop()
+
 
